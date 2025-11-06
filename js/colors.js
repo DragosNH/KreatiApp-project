199e207657841eb7aa8e7409027f95456369a48e
Dragoş <edragos33@gmail.com>
2025-11-06 11:23:28 +0100

    added random keys and values

--- js/colors.js
+++ js/colors.js
@@ -1,10 +1,26 @@
-const colors = new Map();
-colors.set("red", "#ff0000");
-colors.set("green", "#00ff00");
-colors.set("blue", "#0000ff");
+const colors = new Map([
+    ["red", "#ff0000"],
+    ["green", "#00ff00"],
+    ["blue", "#0000ff"],
+    ["black", "#000000"],
+    ["white", "#ffffff"],
+    ["red-purple", "#e40078"],
+    ["mystic", "#d65282"],
+    ["bright pink", "#ff007f"],
+    ["barbie pink", "#ff3988"],
+    ["dark chocolate", "#3c1321"],
+]);
 
 
-let randIndex = Math.floor(Math.random() * colors.length);
 
 
-console.log(colors.keys());
+let keyNumber = Array.from(colors.keys())
+let randIndexForKey = Math.floor(Math.random() * keyNumber.length);
+
+let valueNumber = Array.from(colors.values())
+let randIndexForValue = Math.floor(Math.random() * valueNumber.length);
+
+
+console.log("Colors size is: " + colors.size);
+console.log(valueNumber[randIndexForValue]);
+console.log(keyNumber[randIndexForKey]);
