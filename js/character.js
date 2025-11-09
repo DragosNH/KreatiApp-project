696ea10d0bd0aa18de9972504bec4ce590cf888c
Dragoş <edragos33@gmail.com>
2025-11-09 07:54:19 +0100

    variables declared

---
+++ js/character.js
@@ -0,0 +1,14 @@
+const race = document.querySelector(".race");
+const characterClass = document.querySelector(".class");
+const action = document.querySelector(".action");
+const emotion = document.querySelector(".emotion");
+
+const charRace = [
+    "Human",
+    "Dwarf",
+    "Elf",
+    "Orc",
+    "Hndead",
+    "Hobbit",
+    "Goblin",
+]
