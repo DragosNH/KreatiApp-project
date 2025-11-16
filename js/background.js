11418f63bdcc31cb83bef6397b944ad0a49beda2
Dragoş <edragos33@gmail.com>
2025-11-16 16:29:25 +0100

    declared the variables

--- js/background.js
+++ js/background.js
@@ -1,1 +1,14 @@
-console.log("test")
+const place = document.querySelector(".place");
+const time = document.querySelector(".time");
+
+const places = [
+    "A crouded capital with a castle in the background",
+    "A magical forest with a mysterious source of power",
+    "The ruins of an old castle still haunted by it's previous wars",
+    "The trone room of a powerful king",
+    "A small peaceful village",
+    "A huge storm on a cursed forest",
+    
+]
+
+console.log(places.length);
