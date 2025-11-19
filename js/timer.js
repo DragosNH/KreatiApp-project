12a6bed8496bab47adff1443dfc4572079251a1f
Dragoş <edragos33@gmail.com>
2025-11-19 11:52:51 +0100

    created a one hour function

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,42 @@
-const tenMin = document.querySelector(".tenMin")
+const oneHour = document.querySelector(".oneHour");
+const thirtyMin = document.querySelector(".thirtyMin");
+const tenMin = document.querySelector(".tenMin");
+
+oneHour.addEventListener("click", function(){
+    tenMinCountdown( ".one-countdown", 60, 0 )
+})
 
 tenMin.addEventListener("click", function(){
     tenMinCountdown( ".ten-countdown", 10, 0 )
 })
 
+function oneHourCountdown( e, minutes, seconds )
+{
+    let element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 100000 ) {
+            element.innerHTML = "Time's up!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.querySelector( e );
+    endTime = (+new Date) + 100000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
 
 function tenMinCountdown( e, minutes, seconds )
 {
