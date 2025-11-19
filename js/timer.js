67c4e35a7564260c8d26c3a216c58890c461b5c4
Dragoş <edragos33@gmail.com>
2025-11-19 11:54:50 +0100

    thirty minutes function created

--- js/timer.js
+++ js/timer.js
@@ -6,6 +6,10 @@ oneHour.addEventListener("click", function(){
     tenMinCountdown( ".one-countdown", 60, 0 )
 })
 
+thirtyMin.addEventListener("click", function(){
+    tenMinCountdown( ".thirty-countdown", 30, 0 )
+})
+
 tenMin.addEventListener("click", function(){
     tenMinCountdown( ".ten-countdown", 10, 0 )
 })
@@ -35,6 +39,34 @@ function oneHourCountdown( e, minutes, seconds )
 
     element = document.querySelector( e );
     endTime = (+new Date) + 100000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+function thirtyMinCountdown( e, minutes, seconds )
+{
+    let element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 3000 ) {
+            element.innerHTML = "Time's up!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.querySelector( e );
+    endTime = (+new Date) + 3000 * (60*minutes + seconds) + 500;
     updateTimer();
 }
 
